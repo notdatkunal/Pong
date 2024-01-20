588e96a8d5d8579f83e24a5da2c6f1253b4ee503
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-01-20 17:35:32 +0530

    Constants.java Created

---
+++ src/main/java/org/example/Constants.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Constants {
+    public static final int SCREEEN_WIDTH = 800;
+    public static final int SCREEN_HEIGHT = 600;
+
+    public static final String SCREEN_TITLE = "Pong";
+
+}
