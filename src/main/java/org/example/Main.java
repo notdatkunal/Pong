1f286efec29afd081c7477323c3bba3138672f19
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-01-20 17:34:44 +0530

    Project Initialized

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Window window = new Window();
+        Thread t1 = new Thread();
+        t1.start();
+
+    }
+}
